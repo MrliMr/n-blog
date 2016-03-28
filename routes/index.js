b1c443ed2c3088394c98d2476150376a94c322b2
Aaron <15360085185>
2016-03-28 22:43:48 +0800

    权限控制／新增post

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ module.exports = function (app) {
             error: req.flash('error').toString()
         });
     });
+    app.get('/reg', checkNotLogin);
     app.get('/reg', function (req, res) {
         res.render('reg', {
             title: '注册',
@@ -22,6 +23,7 @@ module.exports = function (app) {
             error: req.flash('error').toString()
         });
     });
+    app.post('/reg', checkNotLogin);
     app.post('/reg', function (req, res) {
         var name = req.body.name,
             password = req.body.password,
@@ -66,6 +68,7 @@ module.exports = function (app) {
             });
         })
     });
+    app.get('/login', checkNotLogin);
     app.get('/login', function (req, res) {
 
         res.render('login', {
@@ -74,6 +77,7 @@ module.exports = function (app) {
             error: req.flash('error').toString()
         });
     });
+    app.post('/login', checkNotLogin);
     app.post('/login', function (req, res) {
         var md5 = crypto.createHash('md5'),
             password = md5.update(req.body.password).digest('hex');
@@ -91,15 +95,38 @@ module.exports = function (app) {
             res.redirect('/');
         })
     });
+    app.get('/post', checkLogin);
     app.get('/post', function (req, res) {
-        res.render('post', {title: '发表'});
+        res.render('post', {
+            title: '发表',
+            user: req.session.user,
+            success: req.flash('success').toString(),
+            error: req.flash('error').toString()
+        });
     });
+    app.post('/post', checkLogin);
     app.post('/post', function (req, res) {
 
     });
     app.get('/logout', function (req, res) {
         req.session.user = null;
-        req.flash('success','登出成功');
+        req.flash('success', '登出成功');
         res.redirect('/');
     });
+
+    function checkNotLogin(req, res, next) {
+        if (req.session.user) {
+            req.flash('error', '已经登录');
+            res.redirect('back');
+        }
+        next();
+    }
+
+    function checkLogin(req, res, next) {
+        if (!req.session.user) {
+            req.flash('error', '未登录!');
+            res.redirect('/login');
+        }
+        next();
+    }
 };
